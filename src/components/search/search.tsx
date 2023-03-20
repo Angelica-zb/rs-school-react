908b60dcd58c27e2d3de7f41d171c481f235bd81
Angelica <angelika.zaborovskaya@gmail.com>
2023-03-20 23:21:25 +0300

    feat:add save search localStorage

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import classes from './search.module.scss';
 
-export default class Search extends React.Component {
+interface ISearch {
+  textSearch: string;
+}
+
+export default class Search extends React.Component<object, ISearch> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      textSearch: localStorage.getItem('LocalStorageSearch') || '',
+    };
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      textSearch: e.target.value,
+    });
+  }
+
+  saveTextSearch = () => {
+    localStorage.setItem('LocalStorageSearch', `${this.state.textSearch}`);
+  };
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.saveTextSearch);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('beforeunload', this.saveTextSearch);
+  }
+
+  public componentDidUpdate(): void {
+    this.saveTextSearch();
+  }
+
   render() {
+    const value = this.state.textSearch;
     return (
       <div className={classes.search}>
-        <input id="search" type="text" />
+        <input id="search" type="text" value={value} onChange={this.changeText} />
         <input type="submit" value="SEARCH" />
       </div>
     );
