c2c6cc71dbf6d47a4bed74994d82f820b4a39d6c
Angelica <angelika.zaborovskaya@gmail.com>
2023-04-03 03:00:59 +0300

    feat: rewrite with the hooks forms

--- src/components/formsBlank/forms.tsx
+++ src/components/formsBlank/forms.tsx
@@ -1,138 +1,172 @@
-import React from 'react';
-import pageData from '../../constants/constants';
+import { useForm } from 'react-hook-form';
 import classes from '../formsBlank/forms.module.scss';
 import { CardData } from '../../constants/cardsData/cardsData';
-import { IErrors, IForms } from 'interfaces/ICardInterfaces';
+import { IForms } from '../../interfaces/ICardInterfaces';
 
-interface IDataForms {
-  onClick: (objData: IForms) => void;
-  clear: boolean;
-  errors: IErrors;
+export interface IForm {
+  name: string;
+  foto: FileList | string;
+  birth: string;
+  fruit: string;
+  male: string;
+  agreement: boolean;
 }
 
-class Forms extends React.Component<IDataForms> {
-  formRef = React.createRef<HTMLFormElement>();
-  nameRef = React.createRef<HTMLInputElement>();
-  fotoRef = React.createRef<HTMLInputElement>();
-  birthRef = React.createRef<HTMLInputElement>();
-  fruitRef = React.createRef<HTMLSelectElement>();
-  maleRef = React.createRef<HTMLInputElement>();
-  femaleRef = React.createRef<HTMLInputElement>();
-  agreementRef = React.createRef<HTMLInputElement>();
+interface IFormDataPage {
+  onSubmitPage: (card: IForms) => void;
+}
 
-  submitForm = (event: React.ChangeEvent<HTMLFormElement>) => {
-    const objData = {
+const Forms = (formData: IFormDataPage) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
       name: '',
       foto: '',
       birth: '',
+      movie: '',
       fruit: '',
       male: '',
       agreement: false,
+    },
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+  });
+
+  const onSubmit = (data: IForm) => {
+    const { name, foto, birth, fruit, male, agreement } = data;
+    const cardData = {
+      name: name,
+
+      foto: URL.createObjectURL(foto[0] as Blob | MediaSource),
+      birth: birth,
+      fruit: fruit,
+
+      male: male,
+      agreement: agreement,
     };
-    event.preventDefault();
-
-    if (this.nameRef.current?.value) {
-      objData.name = this.nameRef.current.value;
-    }
-
-    if (this.fotoRef.current?.value) {
-      objData.foto = this.fotoRef.current!.files?.length
-        ? URL.createObjectURL(this.fotoRef.current!.files![0])
-        : '';
-    }
-
-    if (this.birthRef.current?.value) {
-      objData.birth = this.birthRef.current.value;
-    }
-
-    if (this.fruitRef.current?.value) {
-      objData.fruit = this.fruitRef.current.value;
-    }
-
-    if (this.maleRef.current?.checked) {
-      objData.male = this.maleRef.current.value;
-    }
-
-    if (this.femaleRef.current?.checked) {
-      objData.male = this.femaleRef.current.value;
-    }
-
-    if (this.agreementRef.current?.value) {
-      objData.agreement = this.agreementRef.current.checked;
-    }
-
-    this.props.onClick(objData);
+    alert('Вы в списке');
+    formData.onSubmitPage(cardData);
+    reset();
   };
-  componentDidUpdate(): void {
-    if (this.props.clear) {
-      this.formRef.current?.reset();
-    }
-  }
-  render() {
-    return (
-      <div className={classes.formsContainer}>
-        <form ref={this.formRef} className={classes.forms} onSubmit={this.submitForm}>
-          Любитель фруктов:
-          <div className={classes.name}>
-            Введите имя:
-            <input ref={this.nameRef} type="text" name={pageData.formsField[0]} />
-            {this.props.errors.name && (
-              <p className={classes.active}>Введите корректное имя, минимум три буквы</p>
-            )}
-          </div>
-          <div>
-            Добавьте фото:
-            <input ref={this.fotoRef} type="file" name={pageData.formsField[1]} />
-            {this.props.errors.foto && <p className={classes.active}>Добавьте фото</p>}
-          </div>
-          <div>
-            Введите дату рождения:
-            <input ref={this.birthRef} type="date" name={pageData.formsField[2]} />
-            {this.props.errors.birth && <p className={classes.active}>Вы еще не родились</p>}
-          </div>
-          <div>
-            Выберите любимый фрукт:
-            <select ref={this.fruitRef} name={pageData.formsField[3]}>
-              <option key="0" value="">
-                {''}
+
+  return (
+    <div className={classes.formsContainer}>
+      <form className={classes.forms} onSubmit={handleSubmit(onSubmit)}>
+        Любитель фруктов:
+        <label className={classes.name}>
+          Введите имя:
+          <input
+            type="text"
+            {...register('name', {
+              required: 'Введите имя',
+              minLength: {
+                value: 3,
+                message: 'Mинимум три буквы',
+              },
+            })}
+          />
+        </label>
+        <div className={classes.active}>
+          {errors?.name && (
+            <p>
+              {errors?.name?.message?.toString() || 'Введите корректное имя, минимум три буквы'}
+            </p>
+          )}
+        </div>
+        <label>
+          Добавьте фото:
+          <input
+            type="file"
+            {...register('foto', {
+              required: 'Добавьте фото',
+            })}
+          />
+        </label>
+        <div className={classes.active}>
+          {errors?.foto && <p>{errors?.foto?.message?.toString() || 'Добавьте фото'}</p>}
+        </div>
+        <label>
+          Введите дату рождения:
+          <input
+            type="date"
+            {...register('birth', {
+              required: 'Введите дату рождения',
+              validate: (value) =>
+                new Date(value).getTime() < new Date().getTime() || 'Вы еще не родились',
+            })}
+          />
+        </label>
+        <div className={classes.active}>
+          {errors?.birth && <p>{errors?.birth?.message?.toString() || 'Введите дату рождения'}</p>}
+        </div>
+        <div>
+          Выберите любимый фрукт:
+          <select
+            {...register('fruit', {
+              required: 'Фрукт не выбран',
+            })}
+          >
+            <option key="0" value="">
+              {''}
+            </option>
+            {CardData.map((card) => (
+              <option key={card.id} value={card.name}>
+                {card.name}
               </option>
-              {CardData.map((card) => (
-                <option key={card.id} value={card.name}>
-                  {card.name}
-                </option>
-              ))}
-            </select>
-            {this.props.errors.fruit && <p className={classes.active}>Фрукт не выбран</p>}
+            ))}
+          </select>
+          <div className={classes.active}>
+            {errors?.fruit && <p>{errors?.fruit?.message?.toString()}</p>}
           </div>
-          <div>
-            Пол:
-            <label>
-              <input ref={this.maleRef} type="radio" value="male" name={pageData.formsField[4]} />
-              Мужской
-            </label>
-            <label>
-              <input
-                ref={this.femaleRef}
-                type="radio"
-                value="female"
-                name={pageData.formsField[4]}
-              />
-              Женский
-            </label>
+        </div>
+        <div>
+          Пол:
+          <label>
+            <input
+              type="radio"
+              value="male"
+              {...register('male', {
+                required: 'Пол не выбран',
+              })}
+            />
+            Мужской
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="female"
+              {...register('male', {
+                required: 'Пол не выбран',
+              })}
+            />
+            Женский
+          </label>
+          <div className={classes.active}>
+            {errors?.male && <p>{errors?.male?.message?.toString()}</p>}
           </div>
-          {this.props.errors.male && <p className={classes.active}>Пол не выбран</p>}
-          <div>
-            <label>
-              <input ref={this.agreementRef} type="checkbox" name={pageData.formsField[5]} />
-              Согласен с обработкой данных
-            </label>
-            {this.props.errors.agreement && <p className={classes.active}>Подтвердите согласие</p>}
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              {...register('agreement', {
+                required: 'Подтвердите согласие',
+              })}
+            />
+            Согласен с обработкой данных
+          </label>
+          <div className={classes.active}>
+            {errors?.agreement && <p>{errors?.agreement?.message?.toString()}</p>}
           </div>
-          <input type="submit" value="Отправить" />
-        </form>
-      </div>
-    );
-  }
-}
+        </div>
+        <input type="submit" value="Отправить" />
+      </form>
+    </div>
+  );
+};
 
 export default Forms;
